be1bbb79cbe596315387e0ad19fc22774576ec91
dafhome <d.fernalvarez@gmail.com>
2024-02-14 19:47:43 +0100

    main

--- Ejercicio-3/main.js
+++ Ejercicio-3/main.js
@@ -3,13 +3,15 @@ let radio = parseInt(prompt('¿Cual es el radio de tu círculo?'));
 let area = pi * (radio**2);
 
 console.log('area sin redondear='+area);
+
 area = area.toFixed(4);
 
-document.write('<h3>El área (con decimales) de un círculo con radio '+radio+' es: '+area+'</h3>');
+document.write(`<h3>El área (sin redondear) de un círculo con radio ${radio} es: ${area} </h3>`);
+
 area = Math.round(area);
 
 console.log('pi='+pi);
 console.log('radio)'+radio);
 console.log('area redondeada='+area);
 
-document.write('<h3>El área (redondeada) de un círculo con radio '+radio+' es: '+area+'</h3>');
+document.write(`<h3>El área (redondeada) de un círculo con radio ${radio} es: ${area} </h3>`);
